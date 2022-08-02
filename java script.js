459de602fb2268229c5c9eb20b21bd249334b9c8
Kalyan Adduri <110015689+Developer-Kalyan@users.noreply.github.com>
2022-08-02 18:02:59 +0530

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,8 @@
+function checkDriverAge();
+var first=prompt("your age");
+    if (first <"18"){
+        alert("Sorry, you are too young to drive this car. Powering off");}
+else if(first==="18"){
+    alert("Congratulations on your first year of driving. Enjoy the ride!");}
+else if(first>="18"){
+alert("Powering On. Enjoy the ride!");}
