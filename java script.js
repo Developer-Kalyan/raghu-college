e787d77bb285e7d905fae52c09ead30bd0bd83a6
Kalyan Adduri <110015689+Developer-Kalyan@users.noreply.github.com>
2022-08-02 18:24:41 +0530

    Update java script.js

--- java script.js
+++ java script.js
@@ -1,11 +1,11 @@
-function checkDriverAge();
-var first=prompt("your age");
+function checkDriverAge(){
+    var first = prompt("What is your age?");
     if (first <"18"){
         alert("Sorry, you are too young to drive this car. Powering off");}
 else if(first==="18"){
     alert("Congratulations on your first year of driving. Enjoy the ride!");}
 else if(first>="18"){
 alert("Powering On. Enjoy the ride!");}
+}
 
 checkDriverAge();
-first();
